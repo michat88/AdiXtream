96325ceef2d84f88e0453763affb27982ee7b44f
michat88 <gokilmichat@gmail.com>
2026-03-05 21:25:16 +0900

    Update CS3IPlayer.kt

--- app/src/main/java/com/lagradost/cloudstream3/ui/player/CS3IPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/player/CS3IPlayer.kt
@@ -1218,9 +1218,9 @@ class CS3IPlayer : IPlayer {
                                 videoBufferMs.toInt()
                             },
                             // --- MODIFIKASI: KITA UBAH MENJADI 5000 (5 DETIK) ---
-                            1000,
+                            0000,
                             // --- MODIFIKASI: KITA UBAH MENJADI 5000 (5 DETIK) ---
-                            1000
+                            0000
                         ).build()
                 )
 
