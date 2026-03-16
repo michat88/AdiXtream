33a4fda70b22abc16371a893c11f08b2ed6b8e40
michat88 <gokilmichat@gmail.com>
2026-03-17 03:30:19 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         
-        versionCode = 81
-        versionName = "4.7.4"
+        versionCode = 82
+        versionName = "4.7.5"
 
         // --- PEMBATASAN BAHASA ADIXTREAM ---
         // Hanya memasukkan bahasa Inggris ("en") dan Indonesia ("id" / "in") agar ukuran APK jauh lebih kecil
