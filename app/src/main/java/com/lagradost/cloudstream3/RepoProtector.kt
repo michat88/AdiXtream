543bdd18fee71abe9908060a31f8ccf24e275660
michat88 <gokilmichat@gmail.com>
2026-04-08 10:20:35 +0900

    Update RepoProtector.kt

--- app/src/main/java/com/lagradost/cloudstream3/RepoProtector.kt
+++ app/src/main/java/com/lagradost/cloudstream3/RepoProtector.kt
@@ -14,7 +14,12 @@ object RepoProtector {
         }
     }
 
+    // Repo Premium
     val PREMIUM_REPO_ENCODED = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FtYW5obmI4OC9QcmVtaXVtX1JlcG8vYnVpbGRzL3JlcG8uanNvbg=="
 
+    // Repo Gratis
     val FREE_REPO_ENCODED = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL21pY2hhdDg4L1JlcG9fR3JhdGlzL3JlZnMvaGVhZHMvYnVpbGRzL3JlcG8uanNvbg=="
+    
+    // URL Firebase AdiXtream (Sudah di-encode ke Base64)
+    val FIREBASE_URL_ENCODED = "aHR0cHM6Ly9hZGl4dHJlYW0tcHJlbWl1bS1kZWZhdWx0LXJ0ZGIuYXNpYS1zb3V0aGVhc3QxLmZpcmViYXNlZGF0YWJhc2UuYXBwLw=="
 }
